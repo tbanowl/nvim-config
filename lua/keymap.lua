d8d7c2d755899abe7ce5de04565448c0b6845dda
ZhangLei <tbanowl@gmail.com>
2026-01-13 13:58:20 +0800

    update keymap and add plugin colorizer

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -7,12 +7,18 @@ local keymap = vim.keymap
 -- ------------- 插入模式 -------------
 keymap.set("i", "jj", "<ESC>")
 keymap.set("i", "j;", "<ESC>A;<ENTER>")
+keymap.set("i", "j<ENTER>", "<ESC>A<ENTER>")
+keymap.set("i", "ju", "<ESC>ui")
+keymap.set("i", "jw", "<ESC>wi")
+keymap.set("i", "je", "<ESC>ei")
+keymap.set("i", "jb", "<ESC>bi")
 
 -- ------------- 视觉模式 -------------
 -- 模式：v 视觉模式
--- keymap.set("v", "J", ":m '>+1<CR>gv=gv")
--- keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- ------------- Normal -------------
 -- 模式：n Normal模式
+-- keymap.set("n", "<key>", "<motion>")
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,50 +30,50 @@ require("lazy").setup({
         end,
     },
     -- Vscode-like pictograms
-	{
-		"onsails/lspkind.nvim",
-		event = { "VimEnter" },
-	},
-	-- Auto-completion engine
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"lspkind.nvim",
-			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
-			"hrsh7th/cmp-buffer", -- buffer auto-completion
-			"hrsh7th/cmp-path", -- path auto-completion
-			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
-		},
-		config = function()
-			require("config.nvim-cmp")
-		end,
-	},
-	-- Code snippet engine
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-	},
+    {
+        "onsails/lspkind.nvim",
+        event = { "VimEnter" },
+    },
+    -- Auto-completion engine
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "lspkind.nvim",
+            "hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+            "hrsh7th/cmp-buffer",   -- buffer auto-completion
+            "hrsh7th/cmp-path",     -- path auto-completion
+            "hrsh7th/cmp-cmdline",  -- cmdline auto-completion
+        },
+        config = function()
+            require("config.nvim-cmp")
+        end,
+    },
+    -- Code snippet engine
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+    },
     -- LSP manager
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
     "mfussenegger/nvim-jdtls",
     -- Status line
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("config.lualine")
-		end,
-	},
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("config.lualine")
+        end,
+    },
     -- Autopairs: [], (), "", '', etc
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = function()
-			require("config.nvim-autopairs")
-		end,
-	},
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = function()
+            require("config.nvim-autopairs")
+        end,
+    },
     -- Treesitter
     {
         "nvim-treesitter/nvim-treesitter",
@@ -82,52 +82,51 @@ require("lazy").setup({
         -- event = { "LazyFile", "VeryLazy"},
         lazy = vim.fn.argc(-1) == 0,
         init = function(plugin)
-           require("lazy.core.loader").add_to_rtp(plugin)
-           require("nvim-treesitter.query_predicates")
-
+            require("lazy.core.loader").add_to_rtp(plugin)
+            require("nvim-treesitter.query_predicates")
         end,
-        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall"},
+        cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
         config = function()
             require("config.nvim-treesitter")
         end,
     },
     -- Show indentation and blankline
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		config = function()
-			require("config.indent-blankline")
-		end,
-	},
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        config = function()
+            require("config.indent-blankline")
+        end,
+    },
     -- Better UI
-	-- Run `:checkhealth noice` to check for common issues
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- Add any options here
-		},
-		dependencies = {
-			-- If you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
-		},
+    -- Run `:checkhealth noice` to check for common issues
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- Add any options here
+        },
+        dependencies = {
+            -- If you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        },
         -- config = function()
         --     require("config.nvim-notify")
         -- end,
-	},
+    },
     -- Git integration
-	"tpope/vim-fugitive",
+    "tpope/vim-fugitive",
     -- Git decorations
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("config.gitsigns")
-		end,
-	},
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("config.gitsigns")
+        end,
+    },
     -- im-select
     {
         "keaising/im-select.nvim",
@@ -140,8 +139,63 @@ require("lazy").setup({
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.8",
-        dependencies = { "nvim-lua/plenary.nvim"}
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    -- Markdown、HTML、LaTeX、Typst& YAML 预览
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false,
+    },
+    -- yazi
+    {
+        "mikavilpas/yazi.nvim",
+        version = "*", -- use the latest stable version
+        event = "VeryLazy",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", lazy = true },
+        },
+        keys = {
+            -- 👇 in this section, choose your own keymappings!
+            {
+                "<leader>-",
+                mode = { "n", "v" },
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi at the current file",
+            },
+            {
+                -- Open in the current working directory
+                "<leader>cw",
+                "<cmd>Yazi cwd<cr>",
+                desc = "Open the file manager in nvim's working directory",
+            },
+            {
+                "<c-up>",
+                "<cmd>Yazi toggle<cr>",
+                desc = "Resume the last yazi session",
+            },
+        },
+        ---@type YaziConfig | {}
+        opts = {
+            -- if you want to open yazi instead of netrw, see below for more info
+            open_for_directories = false,
+            keymaps = {
+                show_help = "<f1>",
+            },
+        },
+        -- 👇 if you use `open_for_directories=true`, this is recommended
+        init = function()
+            -- mark netrw as loaded so it's not loaded at all.
+            --
+            -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+            vim.g.loaded_netrwPlugin = 1
+        end,
+    },
+    -- color highlighter
+    {
+        "catgoose/nvim-colorizer.lua",
+        event = "BufReadPre",
+        opts = { -- set to setup table
+        },
     }
-
 
 })
